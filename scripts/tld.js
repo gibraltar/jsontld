b19c97df8801ec9c770cdf0dc8b0c9af05c7fa43
Andrei Odeski <andrei@odeski.ca>
2015-04-10 22:50:31 -0700

    Adding scripts

--- scripts/tld.js
+++ scripts/tld.js
@@ -1,40 +1,71 @@
 $(document).ready(function()
 {
-  $('form').submit(function () {
+ 
+  $("form").submit(function ()
+  {
     return false;
   });
 
-
   // loading json file
-  var url = "./json/tlds.json";
-  
+  var url = "https://cdn.rawgit.com/gibraltar/jsontld/master/json/tlds.json";
+
+
+  $('#goBtn').click(function() {
+    var curName = $("input[name=domainName]").val();
+    goAction(curName);
+  }); 
+
+ 
+
   $('#purchaseBtn').click(function ()
   { 
     window.location.href = $('#domainOptions').val();
   });
       
-  $("input[name=domainName]").keypress(function(e)
+  $("input[name=domainName]").keyup(function(e)
   {
     var curName = $(this).val();
+    if (curName.length === 0) {
+      $('#goBtn').attr('disabled','disabled').show();
+    } else {
+      $('#goBtn').removeAttr('disabled').show();
+    }
+
     if(e.which == 13)
     {
-      $.getJSON( url, function( data )
-      {
-        $.each(data, function (i, val)
-        {
-          var domain = curName + '.' + val.fields.tld
-          var redirect = "http://shop.whynotdomains.com/domains/search.aspx?domainToCheck="+curName+"&tld="+val.fields.tld;
-              
-          $('#domainOptions').append($('<option>', { 
-             value: redirect,
-             text : domain 
-          }));
-        });
-          
-        // show the controls
-        $('#domainOptions').show();
-        $('#purchaseBtn').show();
-      });
+      goAction(curName);
     }
   });
+
+
+  function goAction (curName)
+  {
+    // hide go button
+    $("#goBtn").hide();
+
+    // clear the list
+    $('#domainOptions').find('option').remove().end();
+
+    $.getJSON( url, function( data )
+    {
+      $.each(data, function (i, val)
+      {
+        var domain = curName + val.tld;
+        var redirect = "http://shop.whynotdomains.com/domains/search.aspx?domainToCheck="+curName+"&tld="+val.tld;
+              
+        $('#domainOptions').append($('<option>', { 
+          value: redirect,
+          text : domain 
+        }));
+      });
+          
+      // show the controls
+      $('#domainOptions').show();
+      $('#purchaseBtn').show();
+    });
+  }
+
 });
+
+
+
